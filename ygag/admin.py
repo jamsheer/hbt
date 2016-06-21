19c718daa6bafc97dc688786820757ba15e5467e
jamsheer <jamsheer.kandoth@gmail.com>
2016-06-21 11:20:03 +0400

    aesthetics changes in admin

--- ygag/admin.py
+++ ygag/admin.py
@@ -3,4 +3,14 @@ from django.contrib import admin
 # Register your models here.
 from .models import Name
 
-admin.site.register(Name)
+class NameAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name_text']}),
+        ('Date information', {'fields': ['entry_date']}),
+    ]
+    list_display = ('name_text', 'entry_date')
+    search_fields = ['name_text']
+    list_filter = ['entry_date']
+
+admin.site.register(Name,NameAdmin)
+
