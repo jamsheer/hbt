941a083d701636d716c05c3bf1400349951972f3
jamsheer <jamsheer.kandoth@gmail.com>
2016-06-21 09:16:35 +0400

    Model for Names and option enabled to add names through admin

--- ygag/models.py
+++ ygag/models.py
@@ -3,3 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Name(models.Model):
+    name_text = models.CharField(max_length=200)
+    entry_date = models.DateTimeField('date added')
+
+    def __str__(self):
+        return self.name_text
